f3bba9765f2534918f65aee316ecfde219cac563
Gajanan-Bhat <gajananbhat9@gmail.com>
2024-11-14 11:30:44 +0530

    comitting a project folder

---
+++ first-app/resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+<article>
+    <h1>
+        <a href="/posts/{{$post->slug}}">
+           {{$post->title}}
+        </a>
+      </h1>
+       <div>
+        {!! $post->body !!}
+       </div>
+    </article>
+        <a href="/">Go Back</a>
+</x-layout>
